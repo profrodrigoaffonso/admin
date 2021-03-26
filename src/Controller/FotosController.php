670233bae250603719e184cbc09a0f9ae9dc92ea
Rodrigo Affonso <profrodrigoaffonso@gmnail.com>
2021-03-25 23:33:24 -0300

    Fotos

--- src/Controller/FotosController.php
+++ src/Controller/FotosController.php
@@ -62,18 +62,76 @@ class FotosController extends AppController
                     $image = imagecreatefromjpeg($filename);
                     imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
 
+                    
+
                     // Gerando a imagem de saída para ver no browser, qualidade 75%:
-                // header('Content-Type: image/jpeg');
-                // imagejpeg($image_p, null, 100);
+                    // header('Content-Type: image/jpeg');
+                    // imagejpeg($image_p, null, 100);
 
-                unlink(WWW_ROOT . 'uploads' . DS . $imagem['name']);
+                    unlink(WWW_ROOT . 'uploads' . DS . $imagem['name']);
 
-                $nome_p = str_replace('.jpg', '_tr.jpg', $imagem['name']);
+                    $nome_p = str_replace('.jpg', '_tr.jpg', $imagem['name']);
 
-                // Ou, se preferir, Salvando a imagem em arquivo:
-                imagejpeg($image_p, WWW_ROOT . 'uploads' . DS . $nome_p, 100);
-                
-                //imagejpeg($src, 'uploads/final' . $nome_p, 100);
+                    // Ou, se preferir, Salvando a imagem em arquivo:
+                    imagejpeg($image_p, WWW_ROOT . 'uploads' . DS . $nome_p, 100);
+                    
+                    //imagejpeg($src, 'uploads/final' . $nome_p, 100);
+
+                    /* marca d'agua ********************************************************/
+
+                    // Carregar imagem já existente no servidor
+                    $imagem = imagecreatefromjpeg( WWW_ROOT . 'uploads' . DS . $nome_p );
+                    /* @Parametros
+                    * "foto.jpg" - Caminho relativo ou absoluto da imagem a ser carregada.
+                    */
+                    
+                    // importa um GIF
+                    $imagemLogo = imagecreatefromgif( WWW_ROOT . 'uploads' . DS . 'marcaretrato.gif' );
+                    /*
+                    * Você poderá usar aqui qualquer funcção que retorne de imagem:
+                    * imagecreatefromjpeg, imagecreate, imagecreatetruecolor
+                    * imagecreatefromPNG não funciona direito com transparencia
+                    */
+                    
+                    // Obtem a largura_nova da imagem
+                    $larguraLogo = imagesx( $imagemLogo );
+                    
+                    // Obtém a altura da imagem
+                    $alturaLogo = imagesy( $imagemLogo );
+                    
+                    // Calcula X 5px da latreral direira
+                    $x_logo = imagesx( $imagem ) - $larguraLogo - 5;
+                    
+                    // Calcula X 5px do rodapé
+                    $y_logo = imagesy( $imagem ) - $alturaLogo - 5;
+                    
+                    // Copia a logo para a imagem
+                    imagecopymerge( $imagem, $imagemLogo, $x_logo, $y_logo, 0, 0, $larguraLogo, $alturaLogo, 100 );
+                    /* @Parametros
+                    * $imagem - Imagem previamente criada Usei imagecreatefromjpeg
+                    * $imagemLogo - Imagem previamente criada Usei imagecreatefromgif
+                    * $x_logo - Posição X que a logo será posicionada
+                    * $y_logo - Posição y que a logo será posicionada
+                    * 0 - Posição X da imagem de fundo. Não alterei.
+                    * 0 - Posição Y da imagem de fundo. Não alterei.
+                    * $larguraLogo - Largura da logo
+                    * $alturaLogo - Altura da logo
+                    * 100 - transparencia da logo
+                    */
+                    
+                    // Header informando que é uma imagem JPEG
+                    // header( 'Content-type: image/jpeg' );
+                    
+                    // eEnvia a imagem para o borwser ou arquivo
+                    imagejpeg( $imagem, WWW_ROOT . 'uploads' . DS . $nome_p, 80 );
+                    /* @Parametros
+                    * $imagem - Imagem previamente criada Usei imagecreatefromjpeg
+                    * NULL - O caminho para salvar o arquivo.
+                            Se não definido ou NULL, o stream da imagem será mostrado diretamente.
+                    * 80 - Qualidade da compresão da imagem.
+                    */
+
+                    //die;
 
 
 
